4f173cc4ad6629c3fbed3f669f94f83280d98ded
aasuss <shubhanshaasu@gmail.com>
2026-02-11 23:11:45 +0530

    Ab to ho ja

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/memories/',
+  base: '/',
   plugins: [react()],
   server: {
     allowedHosts: ['972c-115-96-219-113.ngrok-free.app'],
